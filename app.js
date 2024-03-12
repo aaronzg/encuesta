f61d638953cd43eb1a3fdf4c0d085e5b268e13ab
aaronzg <109195375+aaronzg@users.noreply.github.com>
2024-03-11 18:00:15 -0600

    Update

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
+const form = document.querySelector('.form');
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
+    if(localStorage.getItem('send')){
+        alert('Lo sentimos pero no puedes responder la enucesta dos veces');
+    }else{
+        localStorage.setItem('send','true');
+        form.submit();
+    }
+})
